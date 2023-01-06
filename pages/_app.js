4ad43261d0a0db8099f6785fc252227356fbfc49
Vivek <vivekbutola0000@gmail.com>
2023-01-06 21:16:13 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
-import '../styles/globals.css'
+import React from "react";
+import { Toaster } from "react-hot-toast";
+import { Layout } from "../components";
+import "../styles/globals.css";
+import { StateContext } from "../context/StateContext";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  );
 }
+
+export default MyApp;
